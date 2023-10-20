2a10bd24958412f6a3d376ef2fdd364960fa737e
Yash Sharma <112242006+yashsharma044@users.noreply.github.com>
2023-10-20 04:20:48 +0000

    Understanding ofExplicit conversion

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,19 @@
+//Explicit conversion of the data that will come from the source
+let a=10;
+//Now check wheather the data is in the correct datatype
+console.log(typeof a);
+//Suppose it is a string how to change it
+// let score="yash"
+let score="33"
+// All conversion of datatypes First letter is Capital
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33"==>33
+//"33abc"==>NaN==>Not a Number
+//true==>1 false==>0
+//checking on the string
+let val=33
+let newStr=String(val)
+console.log(typeof newStr);
+console.log(newStr);
