5e951272e357a1d00f370128c7312b7407cc30c4
Yash Sharma <112242006+yashsharma044@users.noreply.github.com>
2023-10-20 03:58:06 +0000

    datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";// treat all js code as newwe version
+//alert(3+3); //we are using node it is working on the html document which is under the browser
+//DataTypes in js
+//Number 
+let a=10 //numeric value in the range of 2 power 53 bigInt also use for the larger value //hft
+//string
+let name="yash"
+//boolean
+let accessGiven=true;
+let temp=null;
+console.log(temp);
+//null -->object it means providing empty space
+//undefined-->type -> undefined 
+//symbol-->majorly use for storing unique value
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
