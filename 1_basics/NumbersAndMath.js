7a32c5f2c5021ce328f30185f0f5b479612e6f46
Yash Sharma <112242006+yashsharma044@users.noreply.github.com>
2023-10-23 11:16:22 +0000

    Numbers and Math

---
+++ 1_basics/NumbersAndMath.js
@@ -0,0 +1,25 @@
+const score=100
+// const balance=new Number(1001);
+//Prototype functions of the number
+//changing number to sting
+// console.log(balance.toString());
+// console.log(balance.)
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// ################ Maths in js #######################
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random())//it gives the value in the range of 0 and 1 
+console.log(Math.random()*10+1)//Now the value always comes above 0
+console.log(Math.floor(Math.random()*6+1))
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
+
